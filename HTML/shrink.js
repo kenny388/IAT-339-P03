4013d058cf91d8887847a9bfd0f9e0b04ec08cce
Blah <kennycheung388@yahoo.com.hk>
2018-03-12 19:19:51 -0700

    Made top Bar responsive

--- HTML/shrink.js
+++ HTML/shrink.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   $(this).scrollTop(0);
+
+
 	$(window).scroll(function() {
+     // $(window).resize(function() {
+        if($(window).width() > 580) {
   	if($(document).scrollTop() > 15) {
     	$('#navigation').addClass('shrink');
     }
     else {
     $('#navigation').removeClass('shrink');
+  }
     }
+
+// });
   });
+
+
 });
